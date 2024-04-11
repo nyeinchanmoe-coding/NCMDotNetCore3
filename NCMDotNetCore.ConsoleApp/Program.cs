7e558640213311ef32f30a015bd2bd1bcc0b05df
Nyein Chan Moe <nyeinchanmoe5899@gmail.com>
2024-04-11 14:11:57 +0630

    ADO.Net

--- NCMDotNetCore.ConsoleApp/Program.cs
+++ NCMDotNetCore.ConsoleApp/Program.cs
@@ -1,5 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Data;
 using System.Data.SqlClient;
 
 Console.WriteLine("Hello, World!");
-//SqlConnection
+
+
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = "DESKTOP-C8JODRL";
+stringBuilder.InitialCatalog = "NCMDotNetCore";
+stringBuilder.UserID = "sa";
+stringBuilder.Password = "sa@123";
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+connection.Open();
+Console.WriteLine("Connection Open");
+
+string query = "select * from tbl_blog";
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+
+DataTable dt = new DataTable();
+sqlDataAdapter.Fill(dt);
+
+connection.Close();
+Console.WriteLine("Connection Close");
+
+//dataset => datatable
+//datatable => datarow
+//datarow => datacolumn
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine("Blog Id =>" + dr["BlogId"]);
+    Console.WriteLine("Blog Title =>" + dr["BlogTitle"]);
+    Console.WriteLine("Blog Author =>" + dr["BlogAuthor"]);
+    Console.WriteLine("Blog Content =>" + dr["BlogContent"]);
+    Console.WriteLine("------------------------------------");
+}
+Console.ReadKey();
